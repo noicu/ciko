0ca422258ac2491ffdca314a88c98e3fdb0829b8
Tovi <1018496570@qq.com>
2022-05-17 19:04:19 +0800

    tovi

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,91 @@
 type ProgressCallback = (progress: number) => void;
+type CikoID = string | number | symbol;
+type CikoTaskList = Array<CikoTask>;
+interface CikoTask {
+  [index: string]: any;
+  type: Taskkind;
+}
+
+interface CikoTaskRunConfig {
+  id?: CikoID;
+  async?: boolean;
+}
+
+interface CikoForks {
+  [index: CikoID]: CikoTaskList;
+}
+
+enum Taskkind {
+  delay,
+  event,
+  to,
+  fork,
+}
 
 class Ciko {
-  delay(time?: number, progress?: ProgressCallback):Ciko {
+  constructor(id?: CikoID) {
+    this._befId = id || Symbol();
+    this._forks = {};
+    this._forks[this._befId] = [];
+  }
+  private _forks: CikoForks;
+  private _befId: CikoID;
+
+  private createTask(target: CikoID, task: CikoTask): void {
+    const id = this._befId;
+    this._befId = target;
+    if (!this._forks[id]) this._forks[id] = [];
+    this._forks[id].push(task);
+  }
+
+  // 延迟执行下一步
+  public delay(time?: number, progress?: ProgressCallback): Ciko {
+    this.createTask(this._befId, {
+      type: Taskkind.delay,
+    });
     return this;
   }
 
-  on(event?: Function) {
+  // 事件
+
+  public on(event: string, callback: () => void): Ciko;
+  public on(event: (context: Ciko) => void): Ciko;
+  public on(
+    event: string | ((context: Ciko) => void),
+    callback?: () => void
+  ): Ciko {
+    this.createTask(this._befId, {
+      type: Taskkind.event,
+    });
     return this;
   }
 
-  loop(count?: number) {
+  public run(cikoIds?: (CikoID | CikoTaskRunConfig)[]): void {
+    console.log(cikoIds);
+  }
+
+  // 前往分支
+  public to(id: CikoID, event?: (context: Ciko) => boolean): Ciko {
+    this.createTask(this._befId, {
+      type: Taskkind.to,
+    });
+    if (event) {
+      if (event(this)) {
+        // 跳转
+      }
+    } else {
+      // 跳转
+    }
     return this;
   }
 
-  run() {
+  // 定义分支
+  public fork(id: CikoID): Ciko {
+    this.createTask(id, {
+      type: Taskkind.fork,
+    });
     return this;
   }
 }
 
-function ciko(): Ciko {
-  return new Ciko();
-}
-
-export default ciko;
+export default Ciko;
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,12 +1,34 @@
-import ciko from "../src/index";
+import Ciko from "../src/index";
 
 it("Should Work", () => {
-  ciko()
-    .loop()
+  const ciko = new Ciko();
+
+  ciko
     .delay(2)
-    .on(() => {})
+    .on(() => {
+      console.log(1);
+    })
     .delay(2)
-    .on(() => {})
-    .run();
+    .on("", () => {
+      console.log(1);
+    })
+    .to("111", (c) => false)
+    .to("222");
+
+  ciko
+    .fork("111")
+    .delay(2)
+    .on(() => {
+      console.log(3);
+    });
+
+  ciko
+    .fork("222")
+    .delay(2)
+    .on(() => {
+      console.log(3);
+    });
+
+  ciko.run(["222", "111", {}]);
   // expect(ciko()).toBe('Helloooooo');
 });
